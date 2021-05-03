0523539c319307f255713edeedd66211794ab566
sean <seanpetermcelroy@gmail.com>
2021-05-03 19:28:12 +0100

    Added localStorage for high score

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # Seany's Snake Game
 
-#### I will be trying to rebuild the **first** mobile phone game I ever played. I miss that nokia **fliphone**.
+I will be trying to rebuild the **first** mobile phone game I ever played... I miss that nokia fliphone. (Nokia 6085)
 
 [useInterval() functionality](https://overreacted.io/making-setinterval-declarative-with-react-hooks/)
 
-## Using a linked list to represent the snake
+<br>
 
-## Implemented a HashMap to store the cells that represent a snake
+- Using a linked list to represent the snake
+
+* Implemented a HashMap to store the cells that represent a snake
--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Board from './components/Board'
 import Counter from './components/Counter';
 import Header from './components/Header'
 import GameOver from './components/GameOver'
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 function App() {
   const [score, setScore] = useState(0)
   const [highScore, setHighScore]= useState(0)
   const [gameOver, endGame]= useState(false)
 
+  useEffect(()=>{
+    const storedHighScore=localStorage.getItem('highScore')
+    if(storedHighScore != null) setHighScore(storedHighScore)
+  }, [])
+
   if (score>highScore){
     setHighScore(score)
+    localStorage.setItem('highScore', score)
+    // Set local stoprage
   }
   return (
     <>
